41931121e78f29e147bda48c54e36c27c84d096e
Rix565 <61545240+Rix565@users.noreply.github.com>
2021-04-07 18:45:50 +0200

    [API] Profile service added

--- src/api/v1/index.php
+++ src/api/v1/index.php
@@ -21,6 +21,40 @@ if(!empty($_GET['api_key'])){
 				$send["message"] = "Data recevied";
 				$send['data'] = $request->fetchAll();
 				echo json_encode($send);
+			}else if($_GET['service']=="profile"){
+				if(!empty($_GET['userid'])){
+					try {
+						$q2 = $db->prepare("SELECT * FROM `users` WHERE `id` = :id");
+						$q2->bindParam(":id", $_GET['userid']);
+						$q2->execute();
+						$result2 = $q2->fetch();
+						$q3 = $db->prepare("SELECT * FROM `user_data` WHERE `id`=:id");
+						$q3->execute([
+							"id" => $result2['id']
+						]);
+						$result3 = $q3->fetch();
+					} catch(Exception $e){
+						die("Error ! " . $e->getMessage());	        	
+					}
+					if($result2==true){
+						$send["success"] = true;
+						$send["message"] = "Data recevied";
+						$send['data']['username'] = $result2['username'];
+						$send['data']['skypoints'] = $result3['skypoints'];
+						$send['data']['class'] = $result2['class'];
+						$send['data']['created_date'] = $result2['created_date'];
+						$send['data']['id'] = $result2['id'];
+						echo json_encode($send);
+					}else{
+						$send["success"] = false;
+						$send["message"] = "Invalid user id !";
+						echo json_encode($send);
+					}
+				}else{
+					$send["success"] = false;
+					$send["message"] = "Specify user id !";
+					echo json_encode($send);
+				}
 			}else{
 				$send["success"] = false;
 				$send["message"] = "Unknown service !";
